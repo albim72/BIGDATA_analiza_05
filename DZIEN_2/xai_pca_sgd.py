65bc5fd05dd4b086d4d5af7badbf8abb20f3b58d
albim72 <marcin.albiniak72@gmail.com>
2025-05-13 11:48:20 +0200

    Create xai_pca_sgd.py

---
+++ DZIEN_2/xai_pca_sgd.py
@@ -0,0 +1,64 @@
+#globalna interpretacja cech
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Załaduj dane i dopasuj PCA jak wcześniej
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+data = load_breast_cancer()
+X = data.data
+feature_names = data.feature_names
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+pca = PCA(n_components=3)
+pca.fit(X_scaled)
+
+# PCA komponenty
+components_df = pd.DataFrame(pca.components_, columns=feature_names, index=['PC1', 'PC2', 'PC3'])
+components_df.T.plot.bar(figsize=(14, 6), title="Wpływ cech oryginalnych na PCA")
+plt.ylabel("Waga (ładunek wektora)")
+plt.grid(True)
+plt.tight_layout()
+plt.show()
+
+
+#shap
+
+import shap
+
+# Przekształcamy oryginalne dane treningowe do PCA
+X_pca = pca.transform(X_scaled)
+
+# Dopasuj model
+from sklearn.linear_model import SGDClassifier
+clf = SGDClassifier(loss='hinge')
+clf.fit(X_pca, data.target)
+
+# Obiekt SHAP dla PCA
+explainer = shap.Explainer(clf.predict, X_pca)
+shap_values = explainer(X_pca[:100])  # pierwsze 100 próbek
+
+# Wizualizacja: wartości SHAP
+shap.plots.beeswarm(shap_values)
+
+#lime
+
+import lime
+import lime.lime_tabular
+
+explainer = lime.lime_tabular.LimeTabularExplainer(
+    X_scaled, 
+    feature_names=feature_names,
+    class_names=data.target_names,
+    verbose=True,
+    mode='classification'
+)
+
+# Wyjaśnienie dla jednej próbki
+exp = explainer.explain_instance(X_scaled[0], lambda x: clf.predict(pca.transform(x)))
+exp.show_in_notebook(show_table=True)
