658d0b888c9e44cc7f580aff50cd53d2feb03a72
albim72 <marcin.albiniak72@gmail.com>
2025-05-13 12:01:19 +0200

    Update xai_pca_sgd.py

--- DZIEN_2/xai_pca_sgd.py
+++ DZIEN_2/xai_pca_sgd.py
@@ -51,14 +51,50 @@ shap.plots.beeswarm(shap_values)
 import lime
 import lime.lime_tabular
 
+from sklearn.model_selection import train_test_split
+import numpy as np
+# 1. Wczytanie danych
+data = load_breast_cancer()
+X = data.data
+y = data.target
+feature_names = data.feature_names
+class_names = data.target_names
+
+# 2. Podział na zbiór treningowy i testowy
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# 3. Standaryzacja
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+X_scaled = np.vstack((X_train_scaled, X_test_scaled))
+
+# 4. Redukcja wymiarów (PCA)
+pca = PCA(n_components=5)
+X_train_pca = pca.fit_transform(X_train_scaled)
+X_test_pca = pca.transform(X_test_scaled)
+
+# 5. Klasyfikator – liniowy SVM (SGD)
+clf = SGDClassifier(loss='log_loss', max_iter=1000, tol=1e-3, random_state=42)
+clf.fit(X_train_pca, y_train)
+
+# 6. LIME – wyjaśnianie
 explainer = lime.lime_tabular.LimeTabularExplainer(
-    X_scaled, 
+    X_scaled,
     feature_names=feature_names,
-    class_names=data.target_names,
+    class_names=class_names,
     verbose=True,
     mode='classification'
 )
 
-# Wyjaśnienie dla jednej próbki
-exp = explainer.explain_instance(X_scaled[0], lambda x: clf.predict(pca.transform(x)))
+# 7. Wyjaśnienie konkretnej próbki
+i = 0  # indeks próbki
+exp = explainer.explain_instance(
+    X_scaled[i],
+    lambda x: clf.predict_proba(pca.transform(x))
+)
+
+# 8. Prezentacja wyników
 exp.show_in_notebook(show_table=True)
+fig = exp.as_pyplot_figure()
+plt.show()
